34a4a50781d64f979b6805ded4390e09438c5574
Beatriz Marques <beatrizqmarques@gmail.com>
2024-10-22 09:07:38 -0300

    alterações

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+function Header (){
+     return (
+
+        <>
+            <div className={styles.containerheader}>
+                <img src="" alt="" />
+                <ul>
+                    <li> Benefícios </li>
+                    <li>  </li>
+                    <li> Benefícios </li>
+                    <li> Benefícios </li>
+                </ul>
+
+            </div>
+        </>
+
+     )
+}
+
+export default Header
